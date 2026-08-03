8c89ab7221b4512c368221c124c6b6bda4c8f5cb
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-08-03 14:13:46 +0300

    Update Brightness_control.ino

--- Brightness_control/Brightness_control.ino
+++ Brightness_control/Brightness_control.ino
@@ -1,14 +1,16 @@
 #define potentiometerPIN 35
 #define ledPIN 5
+#define PWM1_Freq  1000
+#define PWM1_Res   12
 
-
+int AN_Pot1_Result = 0;
 void setup() {
   pinMode(potentiometerPIN, INPUT);
   pinMode(ledPIN, OUTPUT);
-  ledcAttach(ledPIN, 5000, 12);
+  ledcAttach(ledPIN, PWM1_Freq, PWM1_Res);
 }
 
 void loop() {
- uint32_t value = analogRead(potentiometerPIN);
- ledcWrite(ledPIN, value);
+ AN_Pot1_Result = analogRead(potentiometerPIN);
+ ledcWrite(ledPIN, AN_Pot1_Result);
 }
