95ff79123b7ff6e2e38b201c14b4ff51cf18ca7d
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-08-03 16:31:20 +0300

    Update Brightness_control.ino
    made that 2V maximum for bright. When 2V reached - max bright appears

--- Brightness_control/Brightness_control.ino
+++ Brightness_control/Brightness_control.ino
@@ -5,6 +5,7 @@
 
 int AN_Pot1_Result = 0;
 void setup() {
+  Serial.begin(115200);
   pinMode(potentiometerPIN, INPUT);
   pinMode(ledPIN, OUTPUT);
   ledcAttach(ledPIN, PWM1_Freq, PWM1_Res);
@@ -12,5 +13,9 @@ void setup() {
 
 void loop() {
  AN_Pot1_Result = analogRead(potentiometerPIN);
+ Serial.println(AN_Pot1_Result);
+ AN_Pot1_Result = map(AN_Pot1_Result, 0, 2480, 0, 4095); 
+ AN_Pot1_Result = constrain(AN_Pot1_Result, 0, 4095);
+ Serial.println(AN_Pot1_Result);
  ledcWrite(ledPIN, AN_Pot1_Result);
 }
