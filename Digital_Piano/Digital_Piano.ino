b557ed75d2a0a62e6660ebcf5a7678fd43f09db1
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-08-06 20:40:21 +0300

    made map. We can find our value using key

--- Digital_Piano/Digital_Piano.ino
+++ Digital_Piano/Digital_Piano.ino
@@ -1,4 +1,7 @@
 #include <Keypad.h>
+#include <iostream>
+#include <map>
+
 #define ROWS  4
 #define COLS  4
 #define piezoPin 13
@@ -13,16 +16,52 @@ char keyMap[ROWS][COLS] = {
 uint8_t rowPins[ROWS] = {14, 27, 26, 25}; // GIOP14, GIOP27, GIOP26, GIOP25
 uint8_t colPins[COLS] = {33, 32, 18, 19}; // GIOP33, GIOP32, GIOP18, GIOP19
 Keypad keypad = Keypad(makeKeymap(keyMap), rowPins, colPins, ROWS, COLS );
+const std::map<char, uint16_t> notes = {
+{'1' , 262},
+{'2' , 294},
+{'3' , 330},
+{'4' , 349},
+{'5' , 392},
+{'6' , 440},
+{'7' , 494},
+{'8' , 523},
+{'9' , 587},
+{'0' , 659},
+{'A' , 698},
+{'B' , 784},
+{'C' , 880},
+{'D' , 988},
+{'*' , 1047},
+{'#' , 1175}
+};
+
 void setup() {
   Serial.begin(115200);
   pinMode(piezoPin, OUTPUT);
 }
 
+uint16_t getFreq(char key){
+   auto it = notes.find(key);
+    if (it != notes.end())
+        return it->second;
+
+    return 0;
+}
+
+void play(uint16_t freq){
+  Serial.println("play");
+  Serial.println(freq);
+  tone(piezoPin, freq, 200);
+}
+
 void loop() {
   char key = keypad.getKey();
-  if (key) {
+  if (key != NO_KEY)
+  {
+    Serial.print("Pressed: ");
     Serial.println(key);
+    uint16_t freq = getFreq(key);
+    Serial.println(freq);
+    play(freq);
   }
-  tone(piezoPin, 349, 3000);
-
 }
