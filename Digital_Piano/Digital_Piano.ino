16138e1d1f1162207ee819ef28e2b87c74474821
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-08-06 19:05:09 +0300

    read key test

--- Digital_Piano/Digital_Piano.ino
+++ Digital_Piano/Digital_Piano.ino
@@ -12,13 +12,17 @@ char keyMap[ROWS][COLS] = {
 
 uint8_t rowPins[ROWS] = {14, 27, 26, 25}; // GIOP14, GIOP27, GIOP26, GIOP25
 uint8_t colPins[COLS] = {33, 32, 18, 19}; // GIOP33, GIOP32, GIOP18, GIOP19
-
+Keypad keypad = Keypad(makeKeymap(keyMap), rowPins, colPins, ROWS, COLS );
 void setup() {
-  Keypad keypad = Keypad(makeKeymap(keyMap), rowPins, colPins, ROWS, COLS );
+  Serial.begin(115200);
   pinMode(piezoPin, OUTPUT);
 }
 
 void loop() {
+  char key = keypad.getKey();
+  if (key) {
+    Serial.println(key);
+  }
   tone(piezoPin, 349, 3000);
 
 }
