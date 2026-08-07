30349325fd1498ca62f45bdd4083d4a1389d7790
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-08-07 16:38:51 +0300

    servo chatter fixed

--- Controlling_Servo_with_Joystick/Controlling_Servo_with_Joystick.ino
+++ Controlling_Servo_with_Joystick/Controlling_Servo_with_Joystick.ino
@@ -2,7 +2,7 @@
 // using mg90s 180°
 #define VRY_PIN 33
 #define VRX_PIN 25
-#define SERVO_X_PIN  13 
+#define SERVO_X_PIN 13 
 
 
 Servo myServo;
@@ -20,20 +20,22 @@ void loop() {
   int valueX = analogRead(VRX_PIN);
   int valueY = analogRead(VRY_PIN);
 
+  if(abs(valueX - 2048) < 50){
+    valueX = 2048;
+  }
   int xAngle = map(valueX, 0, 4095, 0, 180); 
-  
-
   myServo.write(xAngle); 
-
 
   uint32_t current = millis();
   if(current - start > 3000){
     start = current;
-  Serial.println("Joystick: ");
-  Serial.println(valueX);
-  Serial.println(", ");
-  Serial.println(valueY);
-  Serial.println(" => Servo Motor: ");
+  Serial.print("X: ");
+  Serial.print(valueX);
+
+  Serial.print("  Y: ");
+  Serial.print(valueY);
+
+  Serial.print("  Angle: ");
   Serial.println(xAngle);
   }
 }
