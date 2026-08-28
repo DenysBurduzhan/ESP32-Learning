1b349d547b90399594683f4f597571bacf5fc04e
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-08-28 19:14:46 +0300

    removed Arduino lib. Change to low  programming lvl (registers)

--- Traffic_Light/Traffic_Light.ino
+++ Traffic_Light/Traffic_Light.ino
@@ -1,3 +1,5 @@
+#include "soc/gpio_reg.h"
+#include "soc/io_mux_reg.h"
 
 #define yellowLED 12
 #define redLED 13
@@ -12,20 +14,21 @@ void IRAM_ATTR ISR(){
 
 void setup() {
   Serial.begin(9600);
-  pinMode(12, OUTPUT);
-  pinMode(13, OUTPUT);
-  pinMode(14, OUTPUT);
+  REG_SET_BIT(GPIO_ENABLE_REG,
+            (1 << yellowLED) |
+            (1 << redLED) |
+            (1 << greenLED));
   startTime = millis();
-  pinMode(button, INPUT);
-  digitalWrite(button, PULLUP);
+  pinMode(button, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(button), ISR, FALLING);
 }
 
 void buttonClicked(){
   uint32_t currentTime = millis();
-    digitalWrite(12, LOW);
-    digitalWrite(13, HIGH);
-    digitalWrite(14, LOW);
+    REG_WRITE(GPIO_OUT_W1TC_REG, 
+            (1 << yellowLED) |
+            (1 << redLED) |
+            (1 << greenLED));
   if(currentTime - startTime > 10000){
     humanPresent = false;
     startTime = currentTime;
@@ -33,18 +36,23 @@ void buttonClicked(){
 }
 
 void loop() {
-
 uint32_t currentTime = millis();
 if(!humanPresent){
 if(currentTime - startTime <= 1000 ){
-  digitalWrite(13, LOW);
-  digitalWrite(14, HIGH);
+  REG_WRITE(GPIO_OUT_W1TC_REG, 
+            1 << redLED);
+  REG_WRITE(GPIO_OUT_W1TS_REG, 
+            1 << greenLED);
 }else if(currentTime - startTime <= 2000){
-  digitalWrite(14, LOW);
-  digitalWrite(12, HIGH);
+  REG_WRITE(GPIO_OUT_W1TC_REG, 
+            1 << greenLED);
+  REG_WRITE(GPIO_OUT_W1TS_REG, 
+            1 << yellowLED);
 }else if(currentTime - startTime <= 3000){
-  digitalWrite(12, LOW);
-  digitalWrite(13, HIGH);
+  REG_WRITE(GPIO_OUT_W1TC_REG, 
+            1 << yellowLED);
+  REG_WRITE(GPIO_OUT_W1TS_REG, 
+            1 << redLED);
 }else {
   startTime = currentTime;
 }
