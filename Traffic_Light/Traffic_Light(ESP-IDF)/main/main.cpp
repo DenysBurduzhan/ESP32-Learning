e86cbde00023826781ac3a24053b262c091b403b
Denys Burduzhan <151670272+DenysBurduzhan@users.noreply.github.com>
2026-09-06 20:53:02 +0300

    ISR added

--- Traffic_Light/Traffic_Light(ESP-IDF)/main/main.cpp
+++ Traffic_Light/Traffic_Light(ESP-IDF)/main/main.cpp
@@ -7,12 +7,15 @@
 #define redLED GPIO_NUM_13
 #define greenLED GPIO_NUM_14
 #define button GPIO_NUM_26
+
 volatile bool humanPresent = false;
+volatile uint32_t interruptCount = 0;
 
 uint32_t delayTime = 1000;
 
 gpio_num_t leds[] = {yellowLED, redLED, greenLED};
 uint16_t ledsLength = sizeof(leds) / sizeof(leds[0]);
+
 
 int i = 0;
 
@@ -24,17 +27,43 @@ void process(gpio_num_t led_pin, uint32_t delayTime)
     i = (i + 1) % ledsLength;
 }
 
+void buttonClicked(){
+    process(greenLED, delayTime*10);
+}
+
+static void IRAM_ATTR button_isr_handler(void *arg)
+{
+    interruptCount += 1;
+    humanPresent = true;
+}
+
 extern "C" void app_main()
 {
+
+    gpio_config_t io_conf = {
+    .pin_bit_mask = (1ULL << button),
+    .mode = GPIO_MODE_INPUT,
+    .pull_up_en = GPIO_PULLUP_ENABLE,
+    .pull_down_en = GPIO_PULLDOWN_DISABLE,
+    .intr_type = GPIO_INTR_NEGEDGE
+};
+    gpio_config(&io_conf);
+
     gpio_set_direction(yellowLED, GPIO_MODE_OUTPUT);
     gpio_set_direction(redLED, GPIO_MODE_OUTPUT);
     gpio_set_direction(greenLED, GPIO_MODE_OUTPUT);
-    gpio_set_direction(button, GPIO_MODE_INPUT);
-    gpio_set_pull_mode(button, GPIO_PULLUP_ONLY);
+    
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(button, button_isr_handler, NULL);
 
     while (true)
     {
-       
-            process(leds[i], delayTime);
+      if(humanPresent){
+        buttonClicked();
+        printf("%ld\n", interruptCount);
+        humanPresent = false;
+       }else if(!humanPresent){
+        process(leds[i], delayTime);
+       }
     }
 }
